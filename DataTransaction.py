46da8e62b5751c5f9f16e243ec424acd947c2f30
Sanjay Patel <sanjaypatel16@gmail.com>
2023-06-28 01:24:42 -0400

    All files loaded with codes and viz

---
+++ DataTransaction.py
@@ -0,0 +1,335 @@
+# Lets imports all the necessary libraries
+
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType,StructField,StringType,IntegerType,DoubleType,TimestampType,VarcharType
+from pyspark.sql.functions import *
+import requests
+
+
+#create spark session
+spark = SparkSession.builder.appName('Session5').getOrCreate()
+
+
+# load /read the file to create the DF
+branch_df = spark.read.json("TransformedBranchData.json")
+customer_df = spark.read.json("TransformedCustomerData.json")
+credit_df = spark.read.json("TransformedCreditData.json")
+
+
+customer_df = customer_df.select('SSN',
+                                'FIRST_NAME',
+                                'MIDDLE_NAME',
+                                'LAST_NAME',
+                                'Credit_card_no',
+                                'FULL_STREET_ADDRESS',
+                                'CUST_CITY',
+                                'CUST_STATE',
+                                'CUST_COUNTRY',
+                                'CUST_ZIP', 
+                                'CUST_PHONE',
+                                'CUST_EMAIL', 
+                                'LAST_UPDATED')
+
+branch_df = branch_df.select('BRANCH_CODE',
+                            'BRANCH_NAME',
+                            'BRANCH_STREET',
+                            'BRANCH_CITY',
+                            'BRANCH_STATE',
+                            'BRANCH_ZIP',
+                            'BRANCH_PHONE',
+                            'LAST_UPDATED')
+
+credit_df = credit_df.select('CUST_CC_NO',
+                            'TIMEID',
+                            'CUST_SSN',
+                            'BRANCH_CODE',
+                            'TRANSACTION_TYPE',
+                            'TRANSACTION_VALUE',
+                            'TRANSACTION_ID')
+#print(credit_df.show())
+
+
+
+# Function Requirement 1.2 - utilizes Python, PySpark, and Python modules to load data into RDBMS(SQL), perform the following:
+ 
+# a) Create a Database in SQL(MySQL), named “creditcard_capstone.”
+# b) Create a Python and Pyspark Program to load/write the “Credit Card System Data” into RDBMS(creditcard_capstone).
+# Tables should be created by the following names in RDBMS:
+# CDW_SAPP_BRANCH
+# CDW_SAPP_CREDIT_CARD
+# CDW_SAPP_CUSTOMER 
+
+'''
+branch_df.write.format("jdbc") \
+  .mode("append") \
+  .option("url", "jdbc:mysql://localhost:3306/creditcard_capstone") \
+  .option("dbtable", "CDW_SAPP_BRANCH") \
+  .option("user", "root") \
+  .option("password", "Password") \
+  .save()
+
+
+customer_df.write.format("jdbc") \
+  .mode("append") \
+  .option("url", "jdbc:mysql://localhost:3306/creditcard_capstone") \
+  .option("dbtable", "CDW_SAPP_CUSTOMER") \
+  .option("user", "root") \
+  .option("password", "Password") \
+  .save()
+
+credit_df.write.format("jdbc") \
+  .mode("append") \
+  .option("url", "jdbc:mysql://localhost:3306/creditcard_capstone") \
+  .option("dbtable", "CDW_SAPP_CREDIT_CARD") \
+  .option("user", "root") \
+  .option("password", "Password") \
+  .save()
+'''
+
+# 211 Used to display the transactions made by customers living 
+# in a given zip code for a given month and year. Order by day in descending order.
+# 53151  2018  08
+def req211():
+    a = input("Please enter zip code: ")
+    b = input("Enter Year: ") + input("Enter Month: ") + "%"
+
+    result211_df= credit_df.join(customer_df, credit_df.CUST_CC_NO == customer_df.Credit_card_no)\
+                        .select("TRANSACTION_ID", "TRANSACTION_VALUE","TRANSACTION_TYPE","CUST_ZIP","TIMEID")\
+                        .filter((col("CUST_ZIP") == a) & (credit_df.TIMEID.like(b)))\
+                        .dropDuplicates()\
+                        .sort(col("TIMEID").desc())
+    
+    return result211_df
+
+
+# Used to display the number and total values of transactions for a given type.
+def req212():
+    '''display the number and total values of transactions for a given type'''
+    a = input("""\n
+            1. Bills \n
+            2. Healthcare \n
+            3. Gas \n
+            4. Education \n
+            5. Test \n
+            6. Entertainment \n
+            7. Grocery \n
+            .....Enter Number for type of trasaction: """)
+    dict = {"1":"Bills","2":"Healthcare", 
+            "3":"Gas", "4":"Education", 
+            "5":"Test", "6":"Entertainment", 
+            "7":"Grocery"}
+    result212_df = credit_df.filter(credit_df.TRANSACTION_TYPE == dict[a])\
+                        .groupby("TRANSACTION_TYPE")\
+                        .agg(
+                            count("TRANSACTION_TYPE").alias("NUMBER_OF_TRANSACTION"), 
+                            round(sum("TRANSACTION_VALUE"),2).alias("TOTAL_VALUES_OF_TRANSACTION")
+                            )
+    return result212_df
+
+
+
+# 213 Used to display the total number and total values of transactions for branches in a given state.
+def req213():
+    '''display the total number and total values of transactions for branches in a given state'''
+    state_code = input("Please input 2 letters STATE code: ").upper()
+    result213_df =  credit_df.join(branch_df, credit_df.BRANCH_CODE == branch_df.BRANCH_CODE)\
+                        .filter(col("BRANCH_STATE") == state_code)\
+                        .groupby("BRANCH_STATE")\
+                        .agg(
+                            count(credit_df.TRANSACTION_VALUE).alias("Number_of_Transaction"),
+                            round(sum(credit_df.TRANSACTION_VALUE),2).alias("Total_Value_of_Transaction")
+                            )
+    return result213_df
+
+
+
+
+# 221 Used to check the existing account details of a customer.
+# (123)124-3018, 4210653310116272, Wilber Dunham, 123454487
+def req221():
+    '''to check the existing account details of a customer.'''
+    first_name = input("First Name: ")
+    last_name = input("Last Name: ")
+    ssn = input("SSN: ")
+    ccnumber = input("Credit Cart Number: ")
+
+    if len(ssn) != 0 or len(ccnumber) != 0:
+        result221_df = customer_df.filter((col("SSN") == ssn) | (col("Credit_card_no") == ccnumber))
+    elif len(first_name) != 0 and len(last_name) != 0:
+        result221_df= customer_df.filter((col("FIRST_NAME") == first_name) & (col("LAST_NAME") == last_name))\
+                                .dropDuplicates()
+    elif len(first_name) != 0 or len(last_name) != 0:
+        result221_df= customer_df.filter((col("FIRST_NAME") == first_name) | (col("LAST_NAME") == last_name))\
+                                .dropDuplicates()
+    return result221_df
+
+
+
+
+# 222 to modify the existing account details of a customer. ask for enter all numbers you want to update after entering SSN
+# once all numbers entered, split to make list to check if that number is in the list.
+def req222():
+    '''to modify the existing account details of a customer'''
+    ssn = (int(input("Modify Customer Detail. What is SSN of the Customer: ")))
+
+    # you can also use f string below to show current details of customer 
+    details = list(input("enter all letters that needed to be modified (reference below):\n\
+ a. 'FIRST_NAME',\n b. 'MIDDLE_NAME',\n c. 'LAST_NAME',\n d. 'FULL_STREET_ADDRESS',\n e. 'CUST_CITY',\n\
+ f. 'CUST_STATE',\n g. 'CUST_COUNTRY',\n h. 'CUST_ZIP',\n i. 'CUST_PHONE',\n j. 'CUST_EMAIL'\n \
+ ENTER THE LETTER/LETTERS: "))
+    
+    dict1 = {'a':'FIRST_NAME','b':'MIDDLE_NAME','c':'LAST_NAME', 'd':'FULL_STREET_ADDRESS',\
+            'e':'CUST_CITY', 'f':'CUST_STATE', 'g':'CUST_COUNTRY','h':'CUST_ZIP', 'i':'CUST_PHONE', 'j':'CUST_EMAIL'}
+    
+    df2 = customer_df
+    
+    for letter in details:
+        newdetail = (input(f"New {dict1[letter]}: "))
+        df2 = df2.withColumn(dict1[letter], when(col("SSN")== ssn , newdetail).otherwise(df2[dict1[letter]]))
+        df2 = df2.withColumn("LAST_UPDATED", when(col("SSN")== ssn , from_unixtime(current_timestamp().cast("long"))).otherwise(df2.LAST_UPDATED))
+
+    return df2
+    
+
+
+
+# 223 Used to generate a monthly bill for a credit card number for a given month and year.
+# sample: 4210653349028689     2018  08
+def req223():
+    '''to generate a monthly bill for a credit card number for a given month and year.'''
+    billccno = input("Please enter Credit Card number without dashes for monthly bill: ")
+    billyear = input("Enter the Year: ")
+    billmonth = input("Enter the Month: ")
+    timeidlike = billyear + billmonth + "%"
+    firstname = customer_df.filter(col("Credit_card_no") == billccno).head()[1]
+    middlename = customer_df.filter(col("Credit_card_no") == billccno).head()[2]
+    lastname = customer_df.filter(col("Credit_card_no") == billccno).head()[3]
+    addressstreet = customer_df.filter(col("Credit_card_no") == billccno).head()[5]
+    addresscity = customer_df.filter(col("Credit_card_no") == billccno).head()[6]
+    addressstate = customer_df.filter(col("Credit_card_no") == billccno).head()[7]
+    addresszip = customer_df.filter(col("Credit_card_no") == billccno).head()[9]
+                                                                          
+    print("\n \n")
+    print(f"MONTHLY BILL FOR {firstname} {lastname} ")
+    print()
+    # below code can be upgraded to include if statement so month does not become 13 
+    print(f"Billing Period: {billmonth}-01-{billyear} to {int(billmonth) + 1}-01-{billyear}")
+    print()
+    print(firstname," ",middlename.title()," ",lastname)
+    print(addressstreet,",")
+    print(addresscity,", ",addressstate," ",addresszip)
+    print()
+    print("+-------------------------+\n|   MINIMUM PAYMENT DUE   |\n+-------------------------+\n|          $15.00         |\n+-------------------------+")
+    print()
+
+    resultdf = credit_df.filter((col("CUST_CC_NO") == billccno) & (credit_df.TIMEID.like(timeidlike)))\
+            .withColumn("Date", to_date(col("TIMEID"), "yyyyMMdd"))\
+            .select("Date", "TRANSACTION_TYPE", "TRANSACTION_VALUE", "TRANSACTION_ID")
+    resultdf2 = resultdf.agg(round(sum(credit_df.TRANSACTION_VALUE),2).alias("Total_MONTHLY_PAYMENT_DUE"))
+    
+    return resultdf, resultdf2
+
+
+
+# 224 Used to display the transactions made by a customer between two dates. Order by year, month, and day in descending order.
+# 4210653349028689  09-01-2018    12-31-2018
+def req224():
+    '''to display the transactions made by a customer between two dates. Order by year, month, and day in descending order.'''
+    custcc = input("Please enter customer Credit Card Number wihtout Dashes: ")
+    startdate = (input("Enter Starting date MM-DD-YYYY formate: "))
+    enddate = (input("Enter Ending date MM-DD-YYYY formate: "))
+    starting_date = startdate[6:] + startdate[0:2] + startdate[3:5]
+    ending_date = enddate[6:] + enddate[0:2] + enddate[3:5]
+    result224_df = credit_df.filter((col("TIMEID") > starting_date ) & (col("TIMEID") < ending_date ))\
+                            .filter(col("CUST_CC_NO") == custcc)\
+                            .withColumn("Date", to_date(col("TIMEID"), "yyyyMMdd"))\
+                            .sort(col("TIMEID").desc())\
+                            .select("CUST_CC_NO", "Date", "BRANCH_CODE", "TRANSACTION_TYPE", "TRANSACTION_VALUE","TRANSACTION_ID")
+    return result224_df
+
+
+
+def print_menu():
+    '''Print OPTIONS to the user'''
+    print("Please choose from one of the following options:")
+    print("1. TRANSACTION made by customers living in a given ZIP CODE for a given MONTH and YEAR")
+    print("2. The number and total values of TRANSACTIONS for a given TYPE")
+    print("3. Total number and total values of TRANSACTIONS for branches in a given STATE")
+    print("4. Check the existing ACCOUNT DETAILS of a CUSTOMER")
+    print("5. MODIFY the existing ACCOUNT DETAILS of a CUSTOMER")
+    print("6. Gnerate a MONTHLY BILL for a credit card number for a given MONTH and YEAR")
+    print("7. TRANSACTIONS made by a CUSTOMER between two DATES")
+    print("8. EXIT")
+
+
+option = 0 
+
+while True:
+
+    print('''
+
+       /$$ /$$       /$$$$$$$$ /$$   /$$  /$$$$$$  /$$      /$$ /$$$$$$$  /$$       /$$$$$$$$       /$$$$$$$   /$$$$$$  /$$   /$$ /$$   /$$             /$$ /$$      
+      /$$//$$/      | $$_____/| $$  / $$ /$$__  $$| $$$    /$$$| $$__  $$| $$      | $$_____/      | $$__  $$ /$$__  $$| $$$ | $$| $$  /$$/            /$$//$$/      
+     /$$//$$/       | $$      |  $$/ $$/| $$  \ $$| $$$$  /$$$$| $$  \ $$| $$      | $$            | $$  \ $$| $$  \ $$| $$$$| $$| $$ /$$/            /$$//$$/       
+    /$$//$$/        | $$$$$    \  $$$$/ | $$$$$$$$| $$ $$/$$ $$| $$$$$$$/| $$      | $$$$$         | $$$$$$$ | $$$$$$$$| $$ $$ $$| $$$$$/            /$$//$$/        
+   /$$//$$/         | $$__/     >$$  $$ | $$__  $$| $$  $$$| $$| $$____/ | $$      | $$__/         | $$__  $$| $$__  $$| $$  $$$$| $$  $$           /$$//$$/         
+  /$$//$$/          | $$       /$$/\  $$| $$  | $$| $$\  $ | $$| $$      | $$      | $$            | $$  \ $$| $$  | $$| $$\  $$$| $$\  $$         /$$//$$/          
+ /$$//$$/           | $$$$$$$$| $$  \ $$| $$  | $$| $$ \/  | $$| $$      | $$$$$$$$| $$$$$$$$      | $$$$$$$/| $$  | $$| $$ \  $$| $$ \  $$       /$$//$$/           
+|__/|__/            |________/|__/  |__/|__/  |__/|__/     |__/|__/      |________/|________/      |_______/ |__/  |__/|__/  \__/|__/  \__/      |__/|__/            
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+''')
+    print_menu()
+    print()
+    try:
+        option = int(input("Enter Single Digit Option Number: "))
+    
+    except:
+        print("invalid input. Please try again.")
+
+    if option == 1:
+        df211 = req211()
+        df211.show()
+    
+    elif option == 2:
+        df212 = req212()
+        df212.show()
+
+    elif option == 3:
+        df213 = req213()
+        df213.show()
+
+    elif option == 4:
+        df221 = req221()
+        df221.show()
+
+    elif option == 5:
+        df222 = req222()
+        df222.show()
+
+    elif option == 6:
+        df2231, df2232 = req223()
+        df2231.show()
+        df2232.show()
+
+    elif option == 7:
+        df224 = req224()
+        df224.show()
+    
+    elif option == 8:
+        print("\n EXIT OPTION SELECTED \n")
+        break
+    
+    else:
+        option = 0
+        print("Please enter valid input.")
+
+print("\n THANK YOU, HAVE A NICE DAY.\n")
+
+
+
+
+
---
+++ ExtractTransformLoad.py
@@ -0,0 +1,239 @@
+# Lets imports all the necessary libraries
+
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType,StructField,StringType,IntegerType,DoubleType,TimestampType,VarcharType
+from pyspark.sql.functions import *
+import requests
+
+#create spark session
+spark = SparkSession.builder.appName('Session3').getOrCreate()
+
+
+#Extract process
+
+def extract_branch_file(json_file):
+    '''takes branch json file and create spark data frame : branch_df'''
+     # load json file and create spark DF
+    branch_df = spark.read.json(json_file)
+    return branch_df
+
+def extract_customer_file(json_file):
+    '''takes branch json file and create spark data frame : customer_df'''
+     # load json file and create spark DF
+    customer_df = spark.read.json(json_file)
+    return customer_df
+
+def extract_credit_file(json_file):
+    '''takes branch json file and create spark data frame : credit_df'''
+     # read file to create df
+    credit_df = spark.read.json(json_file)
+    return credit_df
+
+
+
+# Transform Process
+
+# function to transform branch_df dataframe
+def prepare_branchdf(branch_df):
+    '''Take branch_df for branch as input and return updated DF as per requirements'''
+
+    # fill null zip column values with 99999 according to requirements
+    branch_df.na.fill(99999, ['BRANCH_ZIP'])
+
+    # rearrange columns + create branch_phone format according to requirements: USING INDEX
+    rdd2 = branch_df.rdd.map(lambda x: (x[1],x[2],x[5], x[0], x[4], x[6], \
+                                        '('+x[3][:3]+')' + x[3][3:6] + "-"+ x[3][6:],\
+                                            x[7]))
+
+    # rename the columns according to requirements
+    df2 = rdd2.toDF([
+    'BRANCH_CODE',
+    'BRANCH_NAME',
+    'BRANCH_STREET',
+    'BRANCH_CITY',
+    'BRANCH_STATE',
+    'BRANCH_ZIP',
+    'BRANCH_PHONE',
+    'LAST_UPDATED'])
+    
+
+    # update columns type according to requirements 
+    updated_branch_df = df2.withColumn("BRANCH_CODE",df2["BRANCH_CODE"].cast(IntegerType()))\
+        .withColumn("BRANCH_NAME",df2["BRANCH_NAME"].cast(VarcharType(40)))\
+        .withColumn("BRANCH_STREET",df2["BRANCH_STREET"].cast(VarcharType(40)))\
+        .withColumn("BRANCH_CITY",df2["BRANCH_CITY"].cast(VarcharType(40)))\
+        .withColumn("BRANCH_STATE",df2["BRANCH_STATE"].cast(VarcharType(40)))\
+        .withColumn("BRANCH_ZIP",df2["BRANCH_ZIP"].cast(IntegerType()))\
+        .withColumn("BRANCH_PHONE",df2["BRANCH_PHONE"].cast(VarcharType(40)))\
+        .withColumn("LAST_UPDATED",df2["LAST_UPDATED"].cast(TimestampType()))
+    
+    return updated_branch_df
+
+
+# function to create customer json file and return spark datafram as per requirement
+def prepare_customerdf(customer_df):
+    '''Prepare customer dataframe from customer_df'''
+    
+    # read CSV file that contains cities and phone area code
+    df_cities = spark.read.csv('us-area-code-cities.csv', header=False)
+    # since the CSV file have unnecessary columns, select first 2 columns that contains area code and cities name
+    df_cities2 = df_cities.select(df_cities.columns[:2])
+    # naming the columns
+    df_cities3 = df_cities2.toDF('AREA_CODE', 'CITIES')
+    # since the target DF CUST_CITY have cities name without spaces, lets remove spaces from here also to match
+    df_cities3 = df_cities3.withColumn('CITIES', regexp_replace('CITIES', ' ', ''))
+
+
+    # since CUST_PHONE column is int type, convert in str to be able to modify later
+    customer_df = customer_df.withColumn("CUST_PHONE",customer_df["CUST_PHONE"].cast(StringType()))
+    # make sure there is not city name with space, so remove spaces
+    customer_df = customer_df.withColumn('CUST_CITY', regexp_replace('CUST_CITY', ' ', ''))
+
+
+    # create the left from of customer df and df cities to get area codes in customer df 
+    customer_df = customer_df.join(df_cities3, customer_df.CUST_CITY == df_cities3.CITIES ,"left")
+    # we still have about 300/952 null values in area code, replace it with 123
+    customer_df= customer_df.na.fill(value="123", subset=['AREA_CODE'])
+    customer_df = customer_df.na.fill(99999, ['CUST_ZIP'])
+
+
+    # create RDD to map each colum and transform as per requeirements:USING COLUMN NAME
+    rdd3 = customer_df.rdd.map(lambda x: \
+                               (x["SSN"], x["FIRST_NAME"].title(), x["MIDDLE_NAME"].lower(),\
+                                x["LAST_NAME"].title(), x["CREDIT_CARD_NO"],\
+                                x["STREET_NAME"]+ ', ' + x["APT_NO"],\
+                                x["CUST_CITY"], x["CUST_STATE"], x["CUST_COUNTRY"], x["CUST_ZIP"],\
+                                '('+ x["AREA_CODE"] +')' + x["CUST_PHONE"][:3] + '-' + x["CUST_PHONE"][3:],\
+                                x["CUST_EMAIL"],x["LAST_UPDATED"]))
+    
+    # Name all the columns according to requirements
+    updated_cust_df = rdd3.toDF([
+                                'SSN',
+                                'FIRST_NAME',
+                                'MIDDLE_NAME',
+                                'LAST_NAME',
+                                'Credit_card_no',
+                                'FULL_STREET_ADDRESS',
+                                'CUST_CITY',
+                                'CUST_STATE',
+                                'CUST_COUNTRY',
+                                'CUST_ZIP', 
+                                'CUST_PHONE',
+                                'CUST_EMAIL', 
+                                'LAST_UPDATED' 
+                                ])
+    
+
+    # update column types according to requirements
+    updated_cust_df = updated_cust_df\
+        .withColumn('SSN',updated_cust_df['SSN'].cast(IntegerType()))\
+        .withColumn('FIRST_NAME',updated_cust_df['FIRST_NAME'].cast(VarcharType(40)))\
+        .withColumn('MIDDLE_NAME',updated_cust_df['MIDDLE_NAME'].cast(VarcharType(40)))\
+        .withColumn('LAST_NAME',updated_cust_df['LAST_NAME'].cast(VarcharType(40)))\
+        .withColumn('Credit_card_no',updated_cust_df['Credit_card_no'].cast(VarcharType(18)))\
+        .withColumn('FULL_STREET_ADDRESS',updated_cust_df['FULL_STREET_ADDRESS'].cast(VarcharType(40)))\
+        .withColumn('CUST_CITY',updated_cust_df['CUST_CITY'].cast(VarcharType(25)))\
+        .withColumn('CUST_STATE',updated_cust_df['CUST_STATE'].cast(VarcharType(25)))\
+        .withColumn('CUST_COUNTRY',updated_cust_df['CUST_COUNTRY'].cast(VarcharType(25)))\
+        .withColumn('CUST_ZIP',updated_cust_df['CUST_ZIP'].cast(IntegerType()))\
+        .withColumn('CUST_PHONE',updated_cust_df['CUST_PHONE'].cast(VarcharType(25)))\
+        .withColumn('CUST_EMAIL',updated_cust_df['CUST_EMAIL'].cast(VarcharType(40)))\
+        .withColumn('LAST_UPDATED',updated_cust_df['LAST_UPDATED'].cast(TimestampType()))
+        
+
+    return updated_cust_df
+
+
+# function to transforn transactional data credit_df
+def prepare_creditdf(credit_df):
+    '''takes credit_df and output requires DF'''
+
+    # convert DAY MONTH and YEAE to string to be able to create YYYYMMDD formate in varchar type
+    credit_df = credit_df\
+        .withColumn('DAY',credit_df['DAY'].cast(StringType()))\
+        .withColumn('MONTH',credit_df['MONTH'].cast(StringType()))\
+        .withColumn('YEAR',credit_df['YEAR'].cast(StringType()))
+    
+    # since some DAY and MONTH values are single digit. lets left pad them with "0"
+    credit_df = credit_df.select('BRANCH_CODE',
+                                    'CREDIT_CARD_NO',
+                                    'CUST_SSN',
+                                    lpad(credit_df.DAY, 2, '0').alias("DAY"),
+                                    lpad(credit_df.MONTH, 2, '0').alias("MONTH"),
+                                    'TRANSACTION_ID',
+                                    'TRANSACTION_TYPE',
+                                    'TRANSACTION_VALUE',
+                                    'YEAR')
+    
+    # create RDD to apply lambda function to 
+    rdd_credit = credit_df.rdd.map(lambda x: (x[1], x[8]+x[4]+x[3] , x[2], x[0],\
+                                        x[6], x[7], x[5]))
+    
+    #convert back to DF with required column names
+    updated_credit_df = rdd_credit.toDF(['CUST_CC_NO',
+                                        'TIMEID',
+                                        'CUST_SSN',
+                                        'BRANCH_CODE',
+                                        'TRANSACTION_TYPE',
+                                        'TRANSACTION_VALUE',
+                                        'TRANSACTION_ID'])
+    
+    # update types of each colums according to requirements
+    updated_credit_df = updated_credit_df\
+        .withColumn('CUST_CC_NO',updated_credit_df['CUST_CC_NO'].cast(VarcharType(20)))\
+        .withColumn('TIMEID',updated_credit_df['TIMEID'].cast(VarcharType(10)))\
+        .withColumn('CUST_SSN',updated_credit_df['CUST_SSN'].cast(IntegerType()))\
+        .withColumn('BRANCH_CODE',updated_credit_df['BRANCH_CODE'].cast(IntegerType()))\
+        .withColumn('TRANSACTION_TYPE',updated_credit_df['TRANSACTION_TYPE'].cast(VarcharType(40)))\
+        .withColumn('TRANSACTION_VALUE',updated_credit_df['TRANSACTION_VALUE'].cast(DoubleType()))\
+        .withColumn('TRANSACTION_ID',updated_credit_df['TRANSACTION_ID'].cast(IntegerType()))
+    
+    return updated_credit_df
+
+
+
+
+def log(message):
+    timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second
+    now = datetime.now() # get current timestamp
+    timestamp = now.strftime(timestamp_format)
+    with open("logfile.txt","a") as f:
+        f.write(timestamp + ',' + message + '\n')
+
+
+
+# ETL PROCESS
+
+log("==========ETL Job Started==========")
+
+log("Extract Phase Started")
+branch_df = extract_branch_file("cdw_sapp_branch.json")
+log("branch file extract: Successful")
+customer_df = extract_customer_file("cdw_sapp_custmer.json")
+log("Customer file extract: Successful")
+credit_df = extract_credit_file("cdw_sapp_credit.json")
+log("Credit file extract: Successful")
+
+
+log("Transform Phase Started")
+transformed_branch_df = prepare_branchdf(branch_df)
+log("branch df transform: Successful")
+transformed_customer_df = prepare_customerdf(customer_df)
+log("customer df transform: Successful")
+transfromed_credit_df = prepare_creditdf(credit_df)
+log("credit df transform: Successful")
+
+
+log("Load Phase Started")
+branch_df.write.mode('overwrite').option("header",True).csv("branchfile")
+log("branch df load: Successful")
+customer_df.write.mode('overwrite').option("header",True).csv("customerfile")
+log("customer df load: Successful")
+credit_df.write.mode('overwrite').option("header",True).csv("creditfile")
+log("credit df load: Successful")
+
+log("=====ETL job completed successfully=====")
